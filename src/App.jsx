ff8ed8e5eb0dc600a6a21d37598160510846adbb
juan escobar <jescobar806@gmail.com>
2021-11-11 17:11:25 -0500

    Se avanza en maquetado de usuarios y avances

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import Index from 'pages/Index';
 import Page2 from 'pages/Page2';
 import IndexCategory1 from 'pages/category1/Index';
 import Category1 from 'pages/category1/CategoryPage1';
+import EstadoUsuarios from 'pages/usuarios/EstadoUsuarios';
 import 'styles/globals.css';
+import 'styles/tabla.css';
+import Inscripcion from 'pages/Inscripcion';
 
 // import PrivateRoute from 'components/PrivateRoute';
 
@@ -29,6 +32,8 @@ function App() {
               <Route path='page2' element={<Page2 />} />
               <Route path='category1' element={<IndexCategory1 />} />
               <Route path='category1/page1' element={<Category1 />} />
+              <Route path='usuarios/estado' element={<EstadoUsuarios />} />
+              <Route path='inscripcion' element={<Inscripcion />} />
             </Route>
           </Routes>
         </BrowserRouter>
