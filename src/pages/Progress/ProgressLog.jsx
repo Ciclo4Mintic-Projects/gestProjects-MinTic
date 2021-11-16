e3c648c0b26291ccc2b46caa05339797042d4a82
Andres r hernandez <frodriguezh@unal.edu.co>
2021-11-15 22:01:11 -0500

    añadiendo estilos al editor de avances

--- src/pages/Progress/ProgressLog.jsx
+++ src/pages/Progress/ProgressLog.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ButtonCircle from 'components/ButtonCircle'
 import diskette from 'assets/diskette.svg'
+import backArrow from 'assets/Arrow.svg'
 const ProgressLog = () => {
+
+  const [content, setContent] = useState("Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minus, modi veritatis consequuntur delectus quia iure incidunt dolorem odio cum, unde velit, harum non laboriosam id quod? Rerum, dolores alias!")
+  const [name, setName] = useState("Nombre del avance")
+
   return (
     <div className=" bg-backgContTem">
       <div className=" mt-36 mx-12">
         <h2 className="text-4xl font-bold  ">Avances</h2>
         <div className="bg-white rounded-3xl mt-10 px-16 py-9">
           <div className="flex justify-between items-center">
-            <div>
-              <p className=" text-grayTem">nombre proyecto</p>
-              <p className=" text-3xl font-bold">nombre avance</p>
+            <div className="w-full">
+              <img src={backArrow} className=" mb-8" />
+              <p className=" text-grayTem mb-4">nombre proyecto</p>
+              <input
+                className="  text-3xl font-bold outline-none w-full"
+                value={name}
+                placeholder="Escribe el nombre del avance"
+                onChange={e => { setName(e.target.value) }}
+              />
             </div>
             <div className="flex">
               <ButtonCircle>
@@ -19,24 +30,32 @@ const ProgressLog = () => {
               <ButtonCircle>+</ButtonCircle>
             </div>
           </div>
-          <h6>specifica acá los aportes de este avance</h6>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minus, modi veritatis consequuntur delectus quia iure incidunt dolorem odio cum, unde velit, harum non laboriosam id quod? Rerum, dolores alias!</p>
-
+          <h6 className=" text-grayTem mt-24 mb-9">Especifica acá los aportes de este avance:</h6>
+          {/* <input className=" w-full h-28" value={content} onChange={e => { setContent(e.target.value) }} /> */}
+          <textarea
+            name=""
+            id=""
+            cols="170"
+            rows="10"
+            onChange={e => { setContent(e.target.value) }}
+            className=" outline-none resize-none">
+            {content}
+          </textarea>
           <div>
-            <h3>Observaciones</h3>
+            <h3 className=" font-bold text-2xl">Observaciones</h3>
             <ul>
-              <li>
-                name <span>Lorem Ipsim dolor</span>
+              <li className=" mt-7 mb-8">
+                <span className="font-bold text-purpleTem">name</span> Lorem Ipsim dolor <br /> <span className=" text-grayTem">Junio 31 2021</span>
               </li>
               <li>
-                name <span>Lorem Ipsim dolor</span>
+                <span className="font-bold text-purpleTem">name</span> Lorem Ipsim dolor <br /> <span className=" text-grayTem">Julio 01 2021</span>
               </li>
             </ul>
           </div>
         </div>
       </div>
 
-    </div >
+    </div>
   )
 }
 
