baab4c63e46c05360ce1ce618fb0c1e8e51b45e3
Carlos Rojas <caranroja@outlook.com>
2021-11-24 17:31:08 -0500

    Se le agregan estilo a la edicion de la inscripcion

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,12 +8,12 @@ const DropDown = ({ label, name, defaultValue = '', required, options }) => {
     setSelectedValue(defaultValue);
   }, [defaultValue]);
   return (
-    <label htmlFor={name} className='flex flex-col my-3'>
-      <span>{label}</span>
+    <label htmlFor={name} className='flex flex-col mb-2'>
+      <span className='font-bold text-lg'>{label}</span>
       <select
         required={required}
         name={name}
-        className='input'
+        className='input-auth'
         value={selectedValue}
         onChange={(e) => setSelectedValue(e.target.value)}
       >
