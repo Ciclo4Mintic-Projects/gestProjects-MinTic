b01e446c8e7eeb43fd3509c39aea4c0ac0c159f0
Andres r hernandez <frodriguezh@unal.edu.co>
2021-12-02 00:49:57 -0500

    se añade mutación de eliminación de avances

--- src/components/ButtonPurple.jsx
+++ src/components/ButtonPurple.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const ButtonPurple = ({ children }) => {
-  return (
-    <button className=" px-4 h-12 bg-purpleTem text-backgContTem rounded-3xl font-semibold hover:bg-purple-400 ">
-      {children}
-    </button>
-  )
+const ButtonPurple = ({ children, eliminar, _id }) => {
+
+
+
+
+  if (eliminar && _id) {
+    return (
+      <button className=" px-4 h-12 bg-purpleTem text-backgContTem rounded-3xl font-semibold hover:bg-purple-400 " onClick={() => { eliminar(_id) }}>
+        {children}
+      </button>
+    )
+  } else {
+    return (
+      <button className=" px-4 h-12 bg-purpleTem text-backgContTem rounded-3xl font-semibold hover:bg-purple-400 " >
+        {children}
+      </button>
+    )
+  }
+
 }
 
 export default ButtonPurple
