28b5f6fa2cab3dfcf922063319a02eec69b3534e
Andres r hernandez <frodriguezh@unal.edu.co>
2021-12-03 15:26:03 -0500

    añadiendo logica para que el lider y el estudiante editen cosas distintas de los avances

--- src/pages/Avances/AvancesLog.jsx
+++ src/pages/Avances/AvancesLog.jsx
@@ -7,6 +7,9 @@ import { useQuery, useMutation } from '@apollo/client'
 import { GET_AVANCE } from 'graphql/avances/queries'
 import useFormData from 'hooks/useFormData'
 import { EDITAR_AVANCE } from 'graphql/avances/mutations';
+import { CREAR_OBSERVACION } from 'graphql/avances/mutations'
+import PrivateComponent from 'components/PrivateComponent'
+
 const AvancesLog = () => {
 
   const { form, formData, updateFormData } = useFormData(null)
@@ -18,6 +21,7 @@ const AvancesLog = () => {
 
   const [editarAvance, { data: mutationData, loading: mutationLoading, error: mutationError }] = useMutation(EDITAR_AVANCE)
 
+  const [crearObservacion, { data: obsData, loading: obsLoading, error: obsError }] = useMutation(CREAR_OBSERVACION)
 
   if (queryLoading) { return <div>cargando...</div> }
 
@@ -30,71 +34,112 @@ const AvancesLog = () => {
     })
   }
 
+  const submitForm2 = (e) => {
+    e.preventDefault();
+    console.log("form datas: ", formData);
+    crearObservacion({
+      variables: { ...formData, _id }
+    })
+
+  }
+
 
   return (
-    <div >
-      <form className=" mx-12" onSubmit={submitForm} ref={form}>
-        <h2 className="text-4xl font-bold  ">Edición de avance</h2>
-        <div className="bg-backgContTem rounded-3xl mt-10 px-16 py-9">
-          <div className="flex justify-between items-center">
-            <div className="w-full">
-              <NavLink to={"/avances"}>
-                <img src={backArrow} className=" mb-8" alt="back arrow icon" />
-              </NavLink>
-              <p className=" text-purpleTem mb-4"><span className=" text-black font-bold">Proyecto</span>  {queryData.Avance.proyecto.nombre}</p>
-              <h6 className=" text-grayTem mt-14 mb-4">Título del avance:</h6>
-              <input
-                className="  text-3xl font-bold outline-none w-full"
-                defaultValue={queryData.Avance.titulo}
-                placeholder="Escribe el nombre del avance"
-                name="titulo"
-              />
-            </div>
-            <div className="flex">
-              <ButtonCircle label="guardar" update={updateFormData}>
-                <img src={diskette} alt="" />
-              </ButtonCircle>
-              <ButtonCircle>+</ButtonCircle>
-            </div>
-          </div>
-          <h6 className=" text-grayTem mt-14 mb-4">Especifica acá los aportes de este avance:</h6>
-          <textarea
-            name="descripcion"
-            id=""
-            cols="170"
-            rows="10"
-            defaultValue={queryData.Avance.descripcion}
-            className=" outline-none resize-none"
-          >
+    <div>
+      <PrivateComponent roleList={["ESTUDIANTE"]} stateList={["AUTORIZADO"]}>
+        <div >
+          <form className=" mx-12" onSubmit={submitForm} ref={form}>
+            <h2 className="text-4xl font-bold  ">Edición de avance</h2>
+            <div className="bg-backgContTem rounded-3xl mt-10 px-16 py-9">
+              <div className="flex justify-between items-center">
+                <div className="w-full">
+                  <NavLink to={"/avances"}>
+                    <img src={backArrow} className=" mb-8" alt="back arrow icon" />
+                  </NavLink>
+                  <p className=" text-purpleTem mb-4"><span className=" text-black font-bold">Proyecto</span>  {queryData.Avance.proyecto.nombre}</p>
+                  <h6 className=" text-grayTem mt-14 mb-4">Título del avance:</h6>
+                  <input
+                    className="  text-3xl font-bold outline-none w-full"
+                    defaultValue={queryData.Avance.titulo}
+                    placeholder="Escribe el nombre del avance"
+                    name="titulo"
+                  />
+                </div>
+                <div className="flex">
+                  <ButtonCircle label="guardar" update={updateFormData}>
+                    <img src={diskette} alt="" />
+                  </ButtonCircle>
+                </div>
+              </div>
+              <h6 className=" text-grayTem mt-14 mb-4">Especifica acá los aportes de este avance:</h6>
+              <textarea
+                name="descripcion"
+                id=""
+                cols="170"
+                rows="10"
+                defaultValue={queryData.Avance.descripcion}
+                className=" outline-none resize-none"
+              >
 
-          </textarea>
-          <div>
-            <h3 className=" font-bold text-2xl">Observaciones</h3>
-            {queryData.Avance.observaciones.length > 0 &&
-              <ul>
-                {queryData.Avance.observaciones.map(observ => {
-                  return (
-                    <li className=" mt-7 mb-8">
-                      <span className="font-bold text-purpleTem">{observ.name}</span> {observ.label} <br /> <span className=" text-grayTem">{observ.date}</span>
-                    </li>
-                  )
-                })}
-              </ul>
-            }
-            {queryData.Avance.observaciones.length == 0 &&
-              <p className=" text-grayTem">No hay observaciones creadas</p>
-            }
-            {
-              queryData.Avance.observaciones.length != 0 && queryData.Avance.observaciones.map(
-                obs => {
-                  return <p>{obs}</p>
+              </textarea>
+              <div>
+                <h3 className=" font-bold text-2xl mb-4">Observaciones</h3>
+                {queryData.Avance.observaciones.length == 0 &&
+                  <p className=" text-grayTem">No hay observaciones creadas</p>
                 }
-              )
-            }
+                {
+                  queryData.Avance.observaciones.length != 0 && queryData.Avance.observaciones.map(
+                    obs => {
+                      return <p>{obs}</p>
+                    }
+                  )
+                }
 
+              </div>
+            </div>
+          </form>
+        </div>
+      </PrivateComponent>
+      <PrivateComponent roleList={["LIDER"]} stateList={["AUTORIZADO"]}>
+        <div >
+          <div className=" mx-12">
+            <h2 className="text-4xl font-bold  ">Avance</h2>
+            <div className="bg-backgContTem rounded-3xl mt-10 px-16 py-9">
+              <div className="flex justify-between items-center">
+                <div className="w-full">
+                  <NavLink to={"/avances"}>
+                    <img src={backArrow} className=" mb-8" alt="back arrow icon" />
+                  </NavLink>
+                  <p className=" text-purpleTem mb-4"><span className=" text-black font-bold">Proyecto</span>  {queryData.Avance.proyecto.nombre}</p>
+                  <p className="  text-3xl font-bold outline-none w-full">{queryData.Avance.titulo}</p>
+                </div>
+              </div>
+              <p className=" outline-none resize-none my-10"> {queryData.Avance.descripcion} </p>
+              <div className="mt-8">
+                <h3 className=" font-bold text-2xl mb-4">Observaciones</h3>
+
+                {queryData.Avance.observaciones.length == 0 &&
+                  <p className=" text-grayTem">No hay observaciones creadas</p>
+                }
+                {
+                  queryData.Avance.observaciones.length != 0 && queryData.Avance.observaciones.map(
+                    obs => {
+                      return <p>{obs}</p>
+                    }
+                  )
+                }
+
+                <form className="mt-6" action="" onSubmit={submitForm2} ref={form}>
+                  <p className="text-grayTem">Escribe alguna observación:</p>
+                  <input type="text" className=" w-96" name="observacion" />
+                  <ButtonCircle update={updateFormData}>Enviar</ButtonCircle>
+                </form>
+
+              </div>
+            </div>
           </div>
         </div>
-      </form>
+      </PrivateComponent>
     </div>
   )
 }
